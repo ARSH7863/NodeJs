577d4c05ff29ff0ec8f19990716f93cc2929a98f
ARSH <arshs287@gmail.com>
2026-05-06 07:32:21 +0530

    Created a new Post route - signup

--- practice/src/app.js
+++ practice/src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
-
 const app = express();
+const User = require("./models/user.js");
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "viratkohli@gmail.com",
+    password: "viratkohli",
+  };
+
+  try {
+    const user = new User(userObj);
+    await user.save();
+
+    res.send(`User added successfully`);
+  } catch (err) {
+    res.status(400).send(`Error saving the user: ${err.message}`);
+  }
+});
 
 connectDB()
   .then(() => {
