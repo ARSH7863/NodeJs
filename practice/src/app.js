1e9da3508435785b4a6fa6d8f77db75648bd096b
ARSH <arshs287@gmail.com>
2026-05-07 08:03:16 +0530

    Created 2 APIs of finding 1 user and entire data of all users

--- practice/src/app.js
+++ practice/src/app.js
@@ -3,21 +3,50 @@ const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
 
+app.use(express.json());
+
+// Creating a new Instance of the User model
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "viratkohli@gmail.com",
-    password: "viratkohli",
-  };
+  // console.log(req.body);
+
+  // const userObj = {
+  //   firstName: "Mahendra Singh",
+  //   lastName: "Dhoni",
+  //   emailId: "msd@gmail.com",
+  //   password: "dhonibhai",
+  // };
 
   try {
-    const user = new User(userObj);
+    const user = new User(req.body);
     await user.save();
-
     res.send(`User added successfully`);
   } catch (err) {
     res.status(400).send(`Error saving the user: ${err.message}`);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+
+    if (users.length === 0) {
+      res.status(404).send(`User Not Found`);
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send(`Something went wrong!`);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send(`Something went wrong!`);
   }
 });
 
