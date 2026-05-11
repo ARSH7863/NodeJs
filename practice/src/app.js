f4a4cd4978152c74b4a229baedd4181f151a10a3
ARSH <arshs287@gmail.com>
2026-05-11 18:54:04 +0530

    Implemented Update & Delete functionality

--- practice/src/app.js
+++ practice/src/app.js
@@ -50,6 +50,28 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send(`User Deleted successfully!`);
+  } catch (err) {
+    res.status(400).send(`User Not Found`);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send(`User Updated Successfully!`);
+  } catch (err) {
+    res.status(400).send(`User Not Found`);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log(`Database connected successfully...`);
