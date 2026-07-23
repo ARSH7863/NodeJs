330301c52ed0e94567f9198403b237f34b8e9fdc
ARSH <arshs287@gmail.com>
2026-07-24 00:12:03 +0530

    Created Connection Request Model and handler

--- practice/apiList.md
+++ practice/apiList.md
@@ -4,17 +4,19 @@
 - POST /login
 - POST /logout
 
+## profileRouter
 - GET /profile/view
 - PATCH /profile/edit
 - PATCH /profile/password // Forgot password API
 
+## connectionRequestRouter
 - POST /request/send/interested/:userId
 - POST /request/send/ignored/:userId
-
 - POST /request/review/accepted/:userId
 - POST /request/review/rejected/:userId
 
+## userRouter
 - GET /connections
 - GET /requests/recieved
 
-Status: ignore, interested, accepted, rejected
+Status: ignored, interested, accepted, rejected
--- practice/src/routes/request.js
+++ practice/src/routes/request.js
@@ -1,11 +1,68 @@
 const express = require("express");
 const requestRouter = express.Router();
+
 const { userAuth } = require("../middleware/auth.js");
+const User = require("../models/user.js");
+const ConnectionRequest = require("../models/connectionRequest.js");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-  res.send(`${user.firstName} has sent a connection request`);
-});
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: `Invalid Status Type: ${status}`,
+        });
+      }
+
+      // If there is an existing ConnectionRequest
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(409)
+          .json({ message: "Connection request already exists!" });
+      }
+
+      if (fromUserId.equals(toUserId)) {
+        return res.status(400).json({
+          message: "You cannot send a connection request to yourself!",
+        });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const toUser = await User.findById(toUserId);
+
+      if (!toUser) {
+        return res.status(400).json({ message: "User Not Found!" });
+      }
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection request sent successfully!",
+        data,
+      });
+    } catch (err) {
+      res.status(400).send(`Error: ${err.message}`);
+    }
+  },
+);
 
 module.exports = requestRouter;
